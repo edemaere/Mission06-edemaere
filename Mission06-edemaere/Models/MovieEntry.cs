faf992d6f2278cc6732ccfc1cf63ad0134fe9ad5
Ezra DeMaere <demaere.ezra@gmail.com>
2023-02-13 22:44:24 -0700

    Add project files.

---
+++ Mission06-edemaere/Models/MovieEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission06_edemaere.Models
+{
+    public class MovieEntry
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Category { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Title { get; set; }
+
+        [Required]
+        public short Year { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Director { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Rating { get; set; }
+
+        public bool Edited { get; set; }
+
+        public string LentTo { get; set; }
+
+        [StringLength(25)]
+        public string Notes { get; set; }
+    }
+}
